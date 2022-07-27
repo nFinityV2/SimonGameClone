4a0edea82b11e4e188836d6c01d3868458b46f75
nFinityV2 <99304246+nFinityV2@users.noreply.github.com>
2022-07-27 13:07:07 +1000

    Finished functionality of game

--- index.js
+++ index.js
@@ -6,31 +6,71 @@ let userPattern = [];
 let level = 0;
 let started = false;
 
+// Function to call the next pattern sequence
 function nextSequence() {
+    userPattern = [];
+    level++;
+
+    document.querySelector("#game-title").innerHTML = "Level  " + level;
+
     var randomNum = Math.floor(Math.random() * btnColours.length);
     var chosenColour = btnColours[randomNum];
     gamePattern.push(chosenColour);
 
-    document.querySelector('#' + chosenColour);
+    animation(chosenColour);
     sound(chosenColour);
-    btnAnimation(chosenColour);
 }
 
-document.addEventListener("keydown", function(){
+// Check the players sequence against the computers
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userPattern[currentLevel]){
+        if (userPattern.length === gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        sound("wrong");
+
+        var wrongBtn = document.querySelector("body");
+        wrongBtn.classList.add("game-over");
+        document.querySelector("#game-title").innerHTML = "GAME OVER! Press any key to restart!"
+
+        setTimeout(() => {
+            wrongBtn.classList.remove("game-over");
+        }, 200);
+    }
+}
+
+// Game Over / Restart game Function
+function reset(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+// Keypress function to initiate game start
+document.addEventListener("keypress",function(){
     if (!started) {
-        document.querySelector("#game-title").innerHTML = "Level " + level;
+        
         nextSequence();
         started = true;
     }
 });
 
+// Button Click Function
+$(".btn").click(function(){
+        var userColour = $(this).attr("id");
+        userPattern.push(userColour);
 
-document.querySelector(".btn").addEventListener("click", function(){
-    
+        sound(userColour);
+        animation(userColour);
+        checkAnswer(userPattern.length-1);
 });
 
-function btnAnimation(pressedKey){
-    var activeBtn = document.querySelector(`.${pressedKey}`);
+// Button Animation Function
+function animation(pressedKey){
+    var activeBtn = document.querySelector("." + pressedKey);
     activeBtn.classList.add("clicked");
 
     setTimeout(function() {
@@ -38,6 +78,7 @@ function btnAnimation(pressedKey){
     }, 200);
 }
 
+// Sound Function
 function sound(colour){
     var colour = new Audio('sounds/' + colour + '.mp3');
     colour.play();
